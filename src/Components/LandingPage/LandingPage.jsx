e1fe58f27ab625fa7e1a5bab374dd5980c823263
Jahanzaibali2 <jahanzaibalikhawaja26@gmail.com>
2024-08-22 18:34:59 +0500

    Landing Page bg set

---
+++ src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './LandingPage.css'
+import Picture from "../../assets/Lp-Home-Aunty.png"
+
+export default function LandingPage() {
+    return (
+        <div className='lp'>
+            <div className="lp-bg">
+                <img src={Picture} alt="." />
+            </div>
+
+        </div>
+    )
+}
